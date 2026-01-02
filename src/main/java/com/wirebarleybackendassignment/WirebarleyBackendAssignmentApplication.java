25df364fa065b89c9e2b587f6118f57fc2f59da4
엄인국 <eum714211@gmail.com>
2026-01-02 17:10:58 +0900

    first commit

---
+++ src/main/java/com/wirebarleybackendassignment/WirebarleyBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.wirebarleybackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirebarleyBackendAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WirebarleyBackendAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wirebarleybackendassignment/WirebarleyBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirebarleybackendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirebarleyBackendAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
